549e27bbe1aae459e67b5e135bfd798ea070bd72
Bruno Tenorio <tenoriobn@gmail.com>
2026-04-04 11:22:30 -0300

    feat: :sparkles: Projeto inicial

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,6 +5,40 @@ import nextTs from "eslint-config-next/typescript";
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
+  {
+    rules: {
+      "no-console": "warn",
+      eqeqeq: ["error", "always"],
+      curly: ["error", "all"],
+      "prefer-const": "error",
+      "object-shorthand": ["error", "always"],
+
+      "no-unused-vars": "off",
+      "@typescript-eslint/no-unused-vars": [
+        "warn",
+        { argsIgnorePattern: "^_" },
+      ],
+      "no-shadow": "off",
+      "@typescript-eslint/no-shadow": "error",
+
+      "no-var": "error",
+      "no-multi-spaces": "error",
+      "no-trailing-spaces": "error",
+
+      "react/jsx-key": "error",
+      "react/self-closing-comp": "warn",
+      "react-hooks/rules-of-hooks": "error",
+      "react-hooks/exhaustive-deps": "warn",
+
+      "react-hooks/set-state-in-effect": "off",
+
+      "@typescript-eslint/consistent-type-imports": [
+        "warn",
+        { prefer: "type-imports" },
+      ],
+      "@typescript-eslint/no-explicit-any": "warn",
+    },
+  },
   // Override default ignores of eslint-config-next.
   globalIgnores([
     // Default ignores of eslint-config-next:
